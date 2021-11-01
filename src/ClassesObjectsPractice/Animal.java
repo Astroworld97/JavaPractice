37567a941fc127d5d1fff5cccf7fde51609aa208
iagonzal97 <iagonzal97@gmail.com>
2021-11-01 11:38:14 -0400

    added some OOP practice and scanner practice as well

---
+++ src/ClassesObjectsPractice/Animal.java
@@ -0,0 +1,14 @@
+package ClassesObjectsPractice;
+
+public class Animal {
+    private String name;
+    private int age;
+    private String species;
+
+    public Animal(String n, int a, String s){ //constructor
+        name = n;
+        age = a;
+        species = s;
+    }
+
+}
